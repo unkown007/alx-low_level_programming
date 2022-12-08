c429b0dbfe9ce3ba58dcd7bcf951d5bae1fa2076
unkown007 <victormatusse00@gmail.com>
2022-12-08 15:48:49 +0200

    6-sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n"
 		"Size of an int: %ld byte(s)\n"
 		"Size of a long int: %ld byte(s)\n"
-		"Size fo a long long int: %ld byte(s)\n"
+		"Size of a long long int: %ld byte(s)\n"
 		"Size of a float: %ld byte(s)\n",
 		sizeof(char),
 		sizeof(int),
