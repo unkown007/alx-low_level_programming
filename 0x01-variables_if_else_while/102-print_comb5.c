06b3fd384e7422aaa2169393437a58bf0eab4df1
unkown007 <victormatusse00@gmail.com>
2022-12-10 00:38:18 +0200

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int i, j, k, l, min, aux;
+
+	min = 48;
+	aux = 1;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = i; k <= 9; k++)
+			{
+				for (l = (aux != 1 ? (j > 0 ? j + 1 : j) : 1);\
+					l <= 9; l++)
+				{
+					putchar(min+i);
+					putchar(min + j);
+					putchar(' ');
+					putchar(min + k);
+					putchar(min + l);
+					if (j <= 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				aux = 0;
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
