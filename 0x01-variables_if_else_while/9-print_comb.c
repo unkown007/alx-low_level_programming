a0a8c78c69772b94e6c81309e3265cb61c4db1f9
unkown007 <victormatusse00@gmail.com>
2022-12-09 22:06:05 +0200

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++) {
+		putchar(c);
+		if ( c < 57 ) {
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
