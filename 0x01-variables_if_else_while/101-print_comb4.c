73581aa7faa65f9c8b961f5516e645c6054c3d88
unkown007 <victormatusse00@gmail.com>
2022-12-09 22:47:39 +0200

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int i, j, k, tmp, tmp1;
+
+	tmp = 9;
+	tmp1 = 8;
+	for (i = 48; i < 56; i++)
+	{
+		for (j = 1; j <= tmp1; j++)
+		{
+			for (k = 2; k <= tmp; k++)
+			{
+				putchar(i);
+				putchar(i + j);
+				putchar(i + k);
+				if (i < 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		tmp1--;
+		tmp--;
+	}
+	putchar('\n');
+
+	return (0);
+}
