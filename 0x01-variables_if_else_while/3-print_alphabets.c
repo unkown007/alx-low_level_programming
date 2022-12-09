23d14a2501283ad1f879a1c2c6a9afa5cc18a3da
unkown007 <victormatusse00@gmail.com>
2022-12-09 21:29:09 +0200

    putchar((char) c);

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+		putchar((char) c);
+
+	for (c = 65; c <= 90; c++)
+		putchar((char) c);
+	putchar('\n');
+
+	return (0);
+}
