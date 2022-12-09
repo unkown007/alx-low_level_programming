24d78ee29b32044dd257ef8023b20e84e4a63f2a
unkown007 <victormatusse00@gmail.com>
2022-12-09 21:21:22 +0200

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int c;
+
+	for(c=97; c <= 122; c++)
+		putchar((char) c);
+	putchar('\n');
+
+	return (0);
+}
