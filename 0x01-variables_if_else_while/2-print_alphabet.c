5bc5148ef5750b7cd7a7c9ed722939a3ad884e49
unkown007 <victormatusse00@gmail.com>
2022-12-09 21:23:46 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (sucess)
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int c;
 
-	for(c=97; c <= 122; c++)
+	for (c=97; c <= 122; c++)
 		putchar((char) c);
 	putchar('\n');
 
