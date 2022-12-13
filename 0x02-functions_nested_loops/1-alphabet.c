05fa5fc6223a94063a99eaccc512e6c84b5ead75
unkown007 <victormatusse00@gmail.com>
2022-12-13 12:40:23 +0200

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ *
+ * Return: Always nothing (void);
+ */
+void print_alphabet(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet()
+{
+        int i;
+
+        for (i = 97; i <= 122; i++)
+		_putchar((char) i);
+	_putchar('\n');
+}
