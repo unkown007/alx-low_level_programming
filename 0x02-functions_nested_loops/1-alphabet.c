de14c6412aacb32e2358549bde49695125830230
unkown007 <victormatusse00@gmail.com>
2022-12-13 12:48:26 +0200

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-void print_alphabet(void);
-
 /**
  * main - Entry point
  *
@@ -11,18 +9,4 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-/**
- * print_alphabet - print the alphabet in lowercase
- *
- * Return: Always nothing (void);
- */
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = 97; i <= 122; i++)
-		_putchar((char) i);
-	_putchar('\n');
 }
