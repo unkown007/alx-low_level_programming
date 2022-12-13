1896413e130cf2a6d7e2db004b6fe4666d9e2cb5
unkown007 <victormatusse00@gmail.com>
2022-12-13 15:36:09 +0200

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -21,3 +21,15 @@ int _isupper(int c)
 	return ((c >= 65) && (c <= 90) ? 1 : 0);
 }
 
+
+/**
+ * _islower - checks wether a c is lowercase or not
+ * @c: character
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return ((c >= 97) && (c <= 122) ? 1 : 0);
+}
+
