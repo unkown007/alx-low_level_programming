e2ded48929df3d907cc3c86a082358c2cf145257
unkown007 <victormatusse00@gmail.com>
2022-12-13 11:23:52 +0200

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for(i=0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+	return (0);
+}
