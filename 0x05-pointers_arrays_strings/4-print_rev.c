8aa90fb22151cb3c5af8c0414747600e50ca5e76
unkown007 <victormatusse00@gmail.com>
2022-12-19 16:15:35 +0200

    print rev done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main"
+
+/**
+ * _strlen - computes length of a string
+ * @s: pointer to char
+ *
+ * Return: lenth the of a string s;
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++ != '\0')
+		length++;
+	return (length);
+}
+
+/**
+ * print_rev - print a string in reverse, followed by a new line
+ * @s: pointer to char
+ */
+void print_rev(char *s)
+{
+	int i;
+	char n = 0x0a;
+
+	for (i = _strlen(s); i >= 0; i--)
+		write(1, &s[i], sizeof(char));
+	write(1, &n, sizeof(char));
+}
