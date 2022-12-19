49b6c29c7ae8610d21679a0387d3ce14ab667248
unkown007 <victormatusse00@gmail.com>
2022-12-19 17:03:37 +0200

    reverse a string done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - computes length of a string
+ * @s: pointer to char
+ *
+ * Return: lenth the of a string s;
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++ != '\0')
+		length++;
+	return (length);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to a char
+ */
+void rev_string(char *s)
+{
+	char *tmp = malloc(sizeof(char) * _strlen(s));
+	int i;
+
+	for (i = 0; (tmp[i] = s[i]) != '\0'; i++)
+		;
+
+	i--;
+	for (; i >= 0; i--)
+		*s++ = tmp[i];
+}
