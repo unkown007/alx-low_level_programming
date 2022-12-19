f82d8723faffbf70247f76b2a77be4386f4ab282
unkown007 <victormatusse00@gmail.com>
2022-12-19 17:32:27 +0200

    puts2 done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _strlen - computes length of a string
+ * @s: pointer to char
+ *
+ * Return: lenth the of a string s;
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++ != '\0')
+		length++;
+	return (length);
+}
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: pointer to char
+ */
+void puts2(char *str)
+{
+	char n = 0x0a;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (((int)str[i]) % 2 == 0)
+			write(1, &str[i], sizeof(char));
+	write(1, &n, sizeof(char));
+}
