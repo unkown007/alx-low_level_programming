eb81be73e2f23f0e3d45960a97e354b5e2ab85cc
unkown007 <victormatusse00@gmail.com>
2022-12-21 17:34:46 +0200

    strcat done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to char
+ * @src: pointer to int
+ *
+ * Return: Always dest (addr);
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while ((dest[i] = *src++) != '\0')
+		i++;
+
+	return (dest);
+}
