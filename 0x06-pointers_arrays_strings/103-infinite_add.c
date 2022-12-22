1a958bea1341db3e6c80cccbeb8bb3444c041a5f
unkown007 <victormatusse00@gmail.com>
2022-12-22 22:59:21 +0200

    fixed

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,84 +12,39 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int sum, i, j, size;
+	int sum, i, j, size, n;
 
 	for (i = 0; n1[i] != '\0'; i++)
 		;
 	for (j = 0; n2[j] != '\0'; j++)
 		;
 
-	i--;
-	j--;
-
-	sum = 0;
-	size = 0;
-	while (i >= 0 && j >= 0)
-	{
-		if (sum)
-			sum = ((n1[i] - '0') + (n2[j] - '0')) + 1;
-		else
-			sum = (n1[i] - '0') + (n2[j] - '0');
-
-		r[size] = (sum % 10) + '0';
-		sum /= 10;
-		i--;
-		j--;
-		size++;
-	}
-
-	if (i >= 0)
-	{
-		while (i >= 0)
-		{
-			if (sum)
-			{
-				sum = (n1[i] - '0') + 1;
-				r[size] = (sum % 10) + '0';
-				sum /= 10;
-			}
-			else
-				r[size] = n1[i];
-			i--;
-			size++;
-		}
-	}
-	else
-	{
-		while (j >= 0)
-		{
-			if (sum)
-			{
-				sum = (n2[j] - '0') + 1;
-				r[size] = (sum % 10) + '0';
-				sum /= 10;
-			}
-			else
-				r[size] = n2[j];
-			j--;
-			size++;
-		}
-	}
-
-	if (sum)
-	{
-		r[size++] = '1';
-	}
-
-	if (size >= size_r)
+	if (i > size_r || j > size_r)
 		return (0);
 
-	i = 0;
-	j = size - 1;
-
-	while (i < j)
+	n = 0;
+	for (i -= 1, j -= 1, size = 0; size < size_r; i--, j--, size++)
 	{
-		sum = r[i];
-		r[i] = r[j];
-		r[j] = sum;
+		sum = n;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
 
-		i++;
-		j--;
+		if (i < 0 && j < 0 && n == 0)
+			break;
+		n = sum / 10;
+		r[size] = (sum % 10) + '0';
+	}
+
+	if (i >= 0 || j >= 0 || sum)
+		return (0);
+
+	for (i = 0, j = size - 1; i < j; i++, j--)
+	{
+		n = r[i];
+		r[i] = r[j];
+		r[j] = n;
 	}
 
 	return (r);
