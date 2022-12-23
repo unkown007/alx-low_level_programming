e849ff10b9bd696d049e8ebf0699aee3d1f1af9c
unkown007 <victormatusse00@gmail.com>
2022-12-23 14:34:51 +0200

    fixed

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,35 +2,52 @@
 #include <stdio.h>
 
 /**
+ * line - print line
+ * @b: buffer
+ * @size: size
+ * @l: line
+ */
+void line(char *b, int size, int l)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= size)
+			printf("%02x", b[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+
+	for (j = 0; j <= size; j++)
+		if (b[l * 10 + j] >= 32 && b[l * 10 + j] <= 126)
+			printf("%c", b[l * 10 + j]);
+		else
+			printf(".");
+}
+
+
+/**
  * print_buffer - prints a buffer
  * @b: pointer to char
  * @size: int
  */
 void print_buffer(char *b, int size)
 {
-	int count, i, j;
+	int i, j;
 
-	for (i = 0, count = 0; i < size;)
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf("%08x: ", count);
+		printf("%08x: ", i * 10);
 
-		j = i;
-		for (j = i; j < (count + 10); j++)
-		{
-			if (j < size)
-				printf("%02x", b[j]);
-			else
-				printf("  ");
-			if (j % 2)
-				printf(" ");
-		}
-
-		for (; i < (count + 10) && i < size; i++)
-			if (b[i] >= 32 && b[i] <= 126)
-				printf("%c", b[i]);
-			else
-				printf(".");
-		count += 10;
-		printf("\n");
+		if (i < size / 10)
+			line(b, 9, i);
+		else
+			line(b, size % 10 - 1, i);
+		putchar('\n');
 	}
+	if (size == 0)
+		putchar('\n');
 }
