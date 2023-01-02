09b52c43c85ac992ffe42e2d9806612e048fbaa9
unkown007 <victormatusse00@gmail.com>
2023-01-02 17:31:25 +0200

    memcpy done

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,7 +14,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n && src[i]; i++)
 		dest[i] = src[i];
-	dest[i] = '\0';
+	
+	if (!src[i])
+		dest[i] = '\0';
 
 	return (dest);
 }
