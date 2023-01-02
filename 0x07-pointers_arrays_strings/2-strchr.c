fa6277b9019a5604166264ab8bb1f2009a8d973c
unkown007 <victormatusse00@gmail.com>
2023-01-02 18:08:40 +0200

    strchr done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,8 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	for (; *s != '\0' && *s != c; ++s)
+		;
 
-	for (i = 0; s[i] != '\0'; i++)
-		if (s[i] == c)
-			return (s + i);
-
-	return (NULL);
+	return (*s == c ? (char *) s : NULL);
 }
