0d99cb050403d0d10bad1e61de01f968dc6c84ad
unkown007 <victormatusse00@gmail.com>
2023-01-02 21:47:54 +0200

    strstr done

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	char *tmp_1, *tmp_2;
 
 	tmp_1 = haystack;
-	while (*tmp_1 && needle)
+	while (*tmp_1 && *needle)
 	{
 		tmp_2 = needle;
 		while (*tmp_1 != *tmp_2 && *tmp_1)
@@ -33,5 +33,5 @@ char *_strstr(char *haystack, char *needle)
 			return ((char *) haystack);
 	}
 
-	return (NULL);
+	return (*needle == '\0' ? haystack : NULL);
 }
