ad6d184417e341da2f09d95b3a16e1b174c0ecfb
unkown007 <victormatusse00@gmail.com>
2023-01-04 21:04:26 +0200

    is prime number done

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * check - check is a number is prime
+ * @i: int
+ * @n: int
+ *
+ * Return: 1 or 0
+ */
+int check(int i, int n)
+{
+	if (i == n)
+		return (1);
+	if ((n % i) == 0 && i < n)
+		return (0);
+	check(i + 1, n);
+}
+
+/**
+ * is_prime_number - checks whether a number is prime
+ * @n: int
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (check(2, n));
+}
