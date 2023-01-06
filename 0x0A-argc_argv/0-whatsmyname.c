3eeaf2f9770dbce536a388d4df00cc76d3148518
unkown007 <victormatusse00@gmail.com>
2023-01-06 14:38:41 +0200

    what is my name done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argv: pointer to an array of char (arguments of the program)
+ * @argc: int (number of the argument)
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
