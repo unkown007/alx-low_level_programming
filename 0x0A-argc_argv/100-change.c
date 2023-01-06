919e106abe2f0c22ae105ee48109e7b6603a87a4
unkown007 <victormatusse00@gmail.com>
2023-01-06 16:26:00 +0200

    change fixed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,6 +3,36 @@
 #include <ctype.h>
 
 /**
+ * coins - compute the minimum numbers of coins for a change
+ * @n: int (number)
+ * @coin: pointer to int
+ * @size: int (size of coin)
+ *
+ * Return: number of coins
+ */
+int coins(int n, int *coin, int size)
+{
+	int i, sum, count;
+
+	sum = count = 0;
+
+	while (sum < n)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if ((sum + coin[i]) <= n)
+			{
+				sum += coin[i];
+				count++;
+				break;
+			}
+		}
+	}
+
+	return (count);
+}
+
+/**
  * main - prints the minimum number of coins to make change for
  * an amount of money
  * @argv: pointer to an array of char (arguments of the program)
@@ -12,7 +42,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum, n, count, coins[] = {25, 10, 5, 2, 1};
+	int  n, coin[] = {25, 10, 5, 2, 1};
+	char *tmp;
 
 	if (argc - 1 != 1)
 	{
@@ -20,30 +51,26 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	sum = 0;
-	n = atoi(*(argv + 1));
+	tmp = *(argv + 1);
 
+	while (*tmp)
+	{
+		if (!isdigit(*tmp) && *tmp != '-')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		tmp++;
+	}
+
+	n = atoi(*(argv + 1));
 	if (n < 0)
 	{
 		printf("0\n");
 		return (1);
 	}
 
-	count = 0;
-	while (sum < n)
-	{
-		for (i = 0; i < 5; i++)
-		{
-			if ((sum + coins[i]) <= n)
-			{
-				sum += coins[i];
-				count++;
-				break;
-			}
-		}
-	}
-
-	printf("%d\n", count);
+	printf("%d\n", coins(n, coin, 5));
 
 	return (0);
 }
