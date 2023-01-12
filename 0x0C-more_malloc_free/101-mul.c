e4e248a600b5b08411841413d278b0f084db6200
unkown007 <victormatusse00@gmail.com>
2023-01-12 16:38:15 +0200

    mult done

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	long n1, n2;
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' && argv[i][j] > '9')
+			{
+				printf("%d - %d Error\n", i, j);
+				exit(98);
+			}
+		}
+	}
+
+	n1 = atol(argv[1]);
+	n2 = atol(argv[2]);
+
+	printf("%ld\n", n1 * n2);
+
+	return (0);
+}
