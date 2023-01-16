b24b30b110f41ff0ab18bb7e4f060b073c06af3d
unkown007 <victormatusse00@gmail.com>
2023-01-16 22:00:26 +0200

    file name done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the source code file name
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
