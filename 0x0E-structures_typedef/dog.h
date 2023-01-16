855eeae617182cad759dd5c29a6c190fc383096e
unkown007 <victormatusse00@gmail.com>
2023-01-16 12:30:25 +0200

    dog data structure done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog definition;
+ * @name: this is the name
+ * @owner: this is the owner
+ * @age: this is age
+ *
+ * Description: this data structure stores information of a dog;
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
