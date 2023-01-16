d63e559ead92e8c3a4579461e6d6d22c179dcc9a
unkown007 <victormatusse00@gmail.com>
2023-01-16 20:06:53 +0200

    fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,39 +15,41 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 	int i;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return (NULL);
-	if (name != NULL)
-	{
+	/*if (name != NULL)
+	{*/
 		d->name = malloc((strlen(name) + 1) * sizeof(char));
 		if (d->name == NULL)
 			return (NULL);
 		for (i = 0; name[i]; i++)
 			d->name[i] = name[i];
 		d->name[i] = '\0';
-	} else
+	/*} else
 	{
 		d->name = malloc(1 * sizeof(char));
 		if (d->name == NULL)
 			return (NULL);
 		d->name[0] = '\0';
-	}
+	}*/
 	d->age = age;
-	if (owner != NULL)
-	{
+	/*if (owner != NULL)
+	{*/
 		d->owner = malloc((strlen(owner) + 1) * sizeof(char));
 		if (d->owner == NULL)
 			return (NULL);
 		for (i = 0; owner[i]; i++)
 			d->owner[i] = owner[i];
 		d->owner[i] = '\0';
-	} else
+	/*} else
 	{
 		d->owner = malloc(1 * sizeof(char));
 		if (d->owner == NULL)
 			return (NULL);
 		d->owner[0] = '\0';
-	}
+	}*/
 	return (d);
 }
