3dcad2c91642fe6a874766da3f93685a904cd192
unkown007 <victormatusse00@gmail.com>
2023-01-18 12:42:29 +0200

    array iterrator done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,11 +6,12 @@
  * as a parameter on each element of an array
  * @array: array of elements
  * @size: size of the array
- * action: pointer to the function
+ * @action: pointer to the function
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	if (array == NULL || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
