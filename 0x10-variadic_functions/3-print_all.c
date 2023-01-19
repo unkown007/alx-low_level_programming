3a2a2c36925298ac42329e438e2d378b09e0dd5f
unkown007 <victormatusse00@gmail.com>
2023-01-19 22:24:19 +0200

    print all fixes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -69,7 +69,7 @@ void print_all(const char * const format, ...)
 	va_start(ap, format);
 	i = 0;
 	separator = "";
-	while (format[i] != '\0' && format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
 		while (j < 4)
