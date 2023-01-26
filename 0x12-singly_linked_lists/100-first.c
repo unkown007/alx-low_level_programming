14b883b48934f5875227f51f896ceabc70a9d04b
unkown007 <victormatusse00@gmail.com>
2023-01-26 22:42:42 +0200

    befire main done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - print a simple text
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
