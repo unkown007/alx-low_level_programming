6d47094dc1631b9bda6f5c49fd11875f857d99c8
unkown007 <victormatusse00@gmail.com>
2023-02-06 21:03:21 +0200

    fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,15 +25,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buff = malloc((letters + 1) * sizeof(char));
+	buff = malloc(letters * sizeof(char));
 	if (buff == NULL)
 		return (0);
 	n = read(fd, buff, letters);
 	if (n == -1)
+	{
+		free(buff);
 		return (0);
+	}
 	m = write(1, buff, n);
 	if (m != n || m == -1)
+	{
+		free(buff);
 		return (0);
+	}
 	close(fd);
 	free(buff);
 	return (n);
