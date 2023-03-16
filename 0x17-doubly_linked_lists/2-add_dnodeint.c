a1194f26681657c6aefb1794251ee3227061bf99
unkown007 <victormatusse00@gmail.com>
2023-03-16 18:03:04 +0200

    fixed errors

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;
 
+	if (head == NULL)
+		return (NULL);
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 		return (NULL);
