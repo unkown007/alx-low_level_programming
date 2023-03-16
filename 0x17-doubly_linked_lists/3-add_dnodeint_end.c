565032e8687640c67c7c83029adaf80c3d70bb6f
unkown007 <victormatusse00@gmail.com>
2023-03-16 18:09:04 +0200

    fixed error

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the end of a dlistint_t list
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
  * @head: head of a linked dlistint_t list
  * @n: integer element to add
  *
