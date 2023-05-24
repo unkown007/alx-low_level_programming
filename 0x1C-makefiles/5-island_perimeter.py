380217f0ed9c5faae3fbd97d01bfea357f4f1d39
unkown007 <victormatusse00@gmail.com>
2023-05-24 21:41:14 +0200

    added file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""This module define two function island_perimeter and square_perimter"""
 
 def square_perimeter(grid, x=0, y=0):
+		"""Computes a perimeter of a grid cell
+		Args:
+				grid(list): matrix of lists
+				x(int): integer
+				y(int): integer
+		Returns:
+				perimeter of the grid cell
+		"""
 		p = 0
 		if (x - 1) < 0 or grid[x - 1][y] == 0:
 				p = p + 1;
@@ -13,6 +22,12 @@ def square_perimeter(grid, x=0, y=0):
 		return (p)
 
 def island_perimeter(grid):
+		"""Computs the perimeter of the island
+		Args:
+				grid(list): matrix of list
+		Return:
+				total perimeter of the island
+		"""
 		perimeter = 0
 		for x in range(len(grid)):
 				for y in range(len(grid[x])):
